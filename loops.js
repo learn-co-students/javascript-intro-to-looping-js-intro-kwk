331d2d648b7e93c82c66f7049933155620762c41
Yvonne Dreptate <yvonnedreptate@gmail.com>
2017-06-18 16:45:06 +0000

    Done.

--- loops.js
+++ loops.js
@@ -4,3 +4,18 @@ function forLoop(array) {
 	}
 	return array
 }
+
+function whileLoop(n) {
+     while (n > 0) {
+          console.log(--n)
+          
+     }
+          return "done"
+     }
+
+function doWhileLoop(array) {
+     do {
+          array.pop()
+     } while (array.length > 0 );
+     return array
+     }
